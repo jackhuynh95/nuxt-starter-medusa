0a32c7dc6cae9e11ad160a6d4ff93da460148348
jackhuynh-nois <truong.huynh@nois.vn>
2025-06-04 15:32:30 +0700

    revert nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,8 +8,7 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxt/image',
     '@nuxt/eslint',
-    // Ignored the deployment to NuxtHub
-    // '@nuxthub/core',
+    '@nuxthub/core',
   ],
   devtools: { enabled: true },
 
